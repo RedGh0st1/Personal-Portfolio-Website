51bb207d62f3c2d30ab693dcaf5351cbabd8be91
Lennie Nurse <lnursej@gmail.com>
2024-08-11 14:44:14 -0400

    nested Hero component inside Home component

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,9 @@
-import Hero from "../../components/Hero/Hero";
+import Hero from "../../components/Home/Hero/Hero";
 import About from "../../components/About/About";
 import Skills from "../../components/Skills/Skills";
 import Projects from "../../components/Projects/Projects";
 import Contact from "../../components/Contact/Contact";
 import Footer from "../../components/Footer/Footer";
-import Home from "../../components/Home/Home";
 
 export default function HomePage() {
   return (
@@ -15,7 +14,6 @@ export default function HomePage() {
       <Projects />
       <Contact />
       <Footer />
-      <Home />
     </div>
   );
 }
