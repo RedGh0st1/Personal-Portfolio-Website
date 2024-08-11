ec33602a87a761ff359d295621fc942300ac07b2
Lennie Nurse <lnursej@gmail.com>
2024-08-11 14:08:08 -0400

    added home to homePage and blogPage template

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -4,6 +4,7 @@ import Skills from "../../components/Skills/Skills";
 import Projects from "../../components/Projects/Projects";
 import Contact from "../../components/Contact/Contact";
 import Footer from "../../components/Footer/Footer";
+import Home from "../../components/Home/Home";
 
 export default function HomePage() {
   return (
@@ -14,6 +15,7 @@ export default function HomePage() {
       <Projects />
       <Contact />
       <Footer />
+      <Home />
     </div>
   );
 }
