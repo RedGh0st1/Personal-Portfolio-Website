c938240abec8cd56b19ffd739ad8463457235ce7
Lennie Nurse <lnursej@gmail.com>
2024-08-11 15:46:01 -0400

    add blog route in routeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./index.css";
 import RouteProvider from "./RouteProvider/RouteProvider";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Home/Hero/Hero";
 
 function App() {
   return (
-    <div className="App_containter">
+    <div className="App_container">
       <Navbar />
       <RouteProvider />
+      <Hero />
     </div>
   );
 }
--- src/RouteProvider/RouteProvider.tsx
+++ src/RouteProvider/RouteProvider.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../Pages/HomePage/HomePage";
+import BlogPage from "../Pages/BlogPage/BlogPage";
 
 export default function RouteProvider() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/blog" element={<BlogPage />} />
       <Route path="*" element={<div> 404 Not Found!!!</div>} />
     </Routes>
   );
