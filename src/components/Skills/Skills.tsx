2a19edd5a0d3b1164317f184fe1125abd1d5eb8b
Lennie Nurse <lnursej@gmail.com>
2024-08-16 00:33:53 -0400

    update css in skill component

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,14 +1,31 @@
+import data from "../../data/data.json";
+import "./Skills.css";
+
 const Skills: React.FC = () => {
   return (
     <section id="myskills" className="skills-section">
       <div className="skills-container">
         <p className="section-title"> Skills</p>
-        <h2 className="skill-section-heading">
+        <h3 className="skill-section-heading">
           {" "}
           Professional <strong className="word-skills">Skills</strong>
-        </h2>
+        </h3>
       </div>
-      <div className="skiils-section-container"></div>
+
+      <div className="skiil-section-container">
+        {/* This div contains the skills content */}
+        {data?.skills.map((skill, index) => (
+          <div className="skill-section-card" key={index}>
+            <div className="skills-section-img">
+              <img src={skill.src} alt="Skills list" />
+            </div>
+
+            <div className="skill-section-card-content">
+              <h3 className="skill-section-title">{skill.title}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
